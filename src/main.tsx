36546b42560d4a043cfecf29b8a9db4593d12f1d
prernashhh <prernaa999@gmail.com>
2026-02-22 12:25:32 +0530

    chore: initialize react-ts project with primereact setup

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primeicons/primeicons.css';
+import './index.css';
+import App from './App.tsx';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
+  </StrictMode>,
+);
